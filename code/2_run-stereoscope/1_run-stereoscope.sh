1330481c0e9ef86f66240df329432ed2d91a12fe
jemorlanes <javier.escudero.morlanes@scilifelab.se>
2025-07-22 16:12:35 +0200

    format stereoscope script

--- code/2_run-stereoscope/1_run-stereoscope.sh
+++ code/2_run-stereoscope/1_run-stereoscope.sh
@@ -2,11 +2,11 @@
 
 export CUDA_VISIBLE_DEVICES=4
 
-stfiles=/home/javier.escudero/stereoscope/run_data/GUT_Helmsley_wLudvig/stsc/ST_counts/Colon/HV/*cleaned.tsv
-scannotation=/home/javier.escudero/stereoscope/run_data/GUT_Helmsley_wLudvig/stsc/single_cell/Colon_Alexandre/updated_sc/HV_mta_data.tsv
-sccounts=/home/javier.escudero/stereoscope/run_data/GUT_Helmsley_wLudvig/stsc/single_cell/Colon_Alexandre/updated_sc/HV_cnt_matrix.tsv
-output=/home/javier.escudero/stereoscope/res/GUT_Helmsley_wLudvig/new_sc_Alexandre/HV_curated_variableGenes
-cellmarkers=/home/javier.escudero/stereoscope/run_data/GUT_Helmsley_wLudvig/stsc/single_cell/Colon_Alexandre/updated_sc/HV_curated_variableGenes.txt
+stfiles=your path to the exported visium counts/*cleaned.tsv
+scannotation=your path to the single cell annotations/HV_mta_data.tsv
+sccounts=your path to the single cell counts/HV_cnt_matrix.tsv
+output=your path to where to save the results
+cellmarkers=your path to the marker genes/HV_curated_variableGenes.txt
 
 stereoscope run \
 	--sc_cnt $sccounts \
